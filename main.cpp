3bf2b64961ad42c057f94733a1c159e306e65965
David Ordnung <git@dordnung.de>
2015-02-18 04:15:37 +0100

    Fixed for current page

--- main.cpp
+++ main.cpp
@@ -284,8 +284,8 @@ void OnGotMainPage(char *error, string result, string url, string data)
 		vector<std::string> founds = splitString(result, "class=\"CurrentPage\">");
 
 
-		// Must be 2
-		if (founds.size() == 2)
+		// Must be 3
+		if (founds.size() == 3)
 		{
 			string found = founds[1];
 
